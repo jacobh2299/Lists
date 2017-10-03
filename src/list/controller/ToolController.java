8760d3aac6a7b5a0a07008e138b88eaabf70016e
jacobh2299 <31513783+jacobh2299@users.noreply.github.com>
2017-10-03 09:32:30 -0600

    created some data members for my donuts

--- src/list/controller/ToolController.java
+++ src/list/controller/ToolController.java
@@ -2,23 +2,31 @@ package list.controller;
 
 import java.util.*;
 import list.model.Donut;
+import list.view.PopupDisplay;
 
 
 public class ToolController 
 {
 	private List<Donut> donutList;
+	private PopupDisplay display;
 	
 	public ToolController()
 	{
 		donutList = new ArrayList<Donut>();
+		display = new PopupDisplay();
 	}
 	
 	public void start() 
 	{
 		Donut temp = new Donut();
 		
-		donutList.add(temp);	
-			
+		donutList.add(temp);			
+	}
+	
+	private void fillTheList()
+	{
+		Donut jellyFilled = new Donut();
+		
 	}
 	
 }
--- src/list/view/PopupDisplay.java
+++ src/list/view/PopupDisplay.java
@@ -4,11 +4,20 @@ import javax.swing.JOptionPane;
 
 public class PopupDisplay 
 {
+	/**
+	 * Displays the supplied message in a popup window.
+	 * @param message
+	 */
 	public void displayText(String message)
 	{
 		JOptionPane.showMessageDialog(null, message);
-		
 	}
+	
+	/**
+	 * Displays the supplied question in a popup and returns the answer as a String
+	 * @param fromQuestion The question to ask the User.
+	 * @return The String Answer
+	 */
 	public String getResponse(String fromQuestion)
 	{
 		String response = "";
