19755ecf2e85bc267420e4af631142cc4ac4664f
jacobh2299 <31513783+jacobh2299@users.noreply.github.com>
2017-10-03 08:33:32 -0600

    created gitignore and made all my packages and classes

---
+++ src/list/controller/ToolController.java
@@ -0,0 +1,5 @@
+package list.controller;
+
+public class ToolController {
+
+}
---
+++ src/list/controller/ToolRunner.java
@@ -0,0 +1,10 @@
+package list.controller;
+
+public class ToolRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
